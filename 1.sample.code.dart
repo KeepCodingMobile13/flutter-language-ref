f3959e56d841c33913483544664446a0166fb539
Fernando Rodriguez <frr@keepcoding.io>
2020-09-01 17:03:22 +0200

    basic intro to some of the features of the alnguage

---
+++ 1.sample.code.dart
@@ -0,0 +1,20 @@
+/*
+Dynamic language, moving to stricter typing.
+All dynamic stuff will be optional via the `dynamic` keyword
+*/
+
+// Function definition
+// If you do not specify the return value, it will be assumed to be dynamic.
+// Always specify your types, or else.
+int successor(int aNumber) {
+  print("The successor of $aNumber is ${++aNumber}");
+  return aNumber; // it was already increased, sucker!
+}
+
+// The entry point: starts a runloop. More on that later
+void main() {
+  var number = 41; // declare, init and type infer
+  // a better way might be...
+  int n = number;
+  print(successor(n));
+}
