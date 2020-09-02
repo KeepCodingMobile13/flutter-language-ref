9b71a2942c387b760e20807393a80d82013cd86d
Fernando Rodriguez <frr@keepcoding.io>
2020-09-02 17:07:13 +0200

    renamed for sorting

---
+++ 8.0.classes.point.dart
@@ -0,0 +1,18 @@
+// Simple yet bad example
+class Point extends Object {
+  var x, y; // son dinámicas y podría meter cualquier cosa
+
+  Point(a, b) {
+    // Mucho rollo y NO llama a super de forma explícita: mucho malo
+    this.x = a;
+    this.y = b;
+  }
+}
+/*
+PEGAS:
+
+I   - Dinámico, sin control de tipos
+II  - No es constante
+III - No sobrescribe igualdad, comparación ni representación en cadena
+IV  - Quien llama a super es Dart ates de empezar la construcción
+*/
