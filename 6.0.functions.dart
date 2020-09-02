b01e2e65291cab8b9eca0e1567015e96bc757b1e
Fernando Rodriguez <frr@keepcoding.io>
2020-09-02 19:30:17 +0200

    reorganized functions and fixed typos

--- 6.0.functions.dart
+++ 6.0.functions.dart
@@ -66,36 +66,3 @@ int badFunction(int n) {
   n = 42; // Eres un gilipollas
   return n + 1;
 }
-
-// High order functions: fully supported
-adder(int n) {
-  int addHelper(int k) => k + n;
-  return addHelper;
-}
-
-// A function cannot be the default value of an optional
-// param (a completion closure) as it wouldn't be a const!
-
-// Named params: come last between {} as name:value pairs.
-// Flutter only uses named params for everything!!!
-
-noop() {} // can pass the body as the value, as it must be determined at compile time
-
-subtract(int a, int b, {ifZero: noop}) {
-  final int res = a - b;
-  if (res == 0) {
-    ifZero();
-  }
-
-  return res;
-}
-
-// Function overloading
-// Not possible, as the type is always the same: Function.
-int k(int n) => n + 1;
-// String k(int n) => "$n";
-
-// In the previous example, if passing a "completion closure" with the wrong
-// type, it would cause a runtime error.
-// TEST YOUR SHIT!
-// USE DOUBLE-DISPATCH!!
---
+++ 6.2.functions.overloading.dart
@@ -0,0 +1,9 @@
+// Function overloading
+// Not possible, as the type is always the same: Function.
+int k(int n) => n + 1;
+// String k(int n) => "$n";
+
+// In the previous example, if passing a "completion closure" with the wrong
+// type, it would cause a runtime error.
+// TEST YOUR SHIT!
+// USE DOUBLE-DISPATCH!!
