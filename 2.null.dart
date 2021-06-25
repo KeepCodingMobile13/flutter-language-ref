f55c3456ecb2aa9ee623c3e29a5a2d08efb4e695
Fernando Rodríguez <frr@keepcoding.io>
2021-06-25 16:51:25 +0200

    fixed accessors

--- 2.null.dart
+++ 2.null.dart
@@ -35,6 +35,19 @@
 /// `a?.foo()` Llama a foo sólo si a NO es null
 /// `v = b ?? 42`  Asigna b a v, a no ser que b sea null. En ese caso, asigna 42
 /// `b ??= 42`  Asigna 42 a b, siempre y cuando b  sea Null
+///
+/// ## Syntactic sugar a lo Kotlin
+///
+/// Se podría crear una "syntactic sugar" para "hacer cosas a una variable, solo
+/// si no es null" como en Kotlin.
+///
+/// Para ello, se usan extensiones son Object?. Ver 8.9 Extensions
+///
+/// ## Asignación late
+///
+/// Con la palabra clave late, indicas que una variable no nulable tendrá un
+/// valor antes de ser usado. No hace falta que lo asignes en el constructor, puede ser
+/// en otro sitio, siempre que lo hagas. Si no lo haces, casca al compilar
 
 void main() {
   int k; // no vale null. No está asignada. Compilará, pero como la uses, casca
@@ -48,4 +61,8 @@ void main() {
   int? b;
   b ??= 42;
   print(b);
+}
+
+class Fistro {
+  late String name;
 }
--- 8.2.classes.accessors.dart
+++ 8.2.classes.accessors.dart
@@ -1,16 +1,16 @@
-/*
-It's impossible to access the iVar directly. Dart compiles a getter 
-and setter with the same name and makes it look like a field access instead of
-function call.
-
-If you want to create your own setter, you need a kludge
-*/
+///
+/// No se pued ejamás acceder directamente a las iVars. Dart sempre genera un
+/// getter y un setter,tanto si quieres como sino.
+///
+///
 
 class ClassyPerson {
-  String _name; // private, but still getter and setter!
+  late String _name; // private, but still getter and setter!
+
+  ClassyPerson(this._name);
 
   // Public Interface
-  get name => "Mr. $_name";
+  String get name => "Mr. $_name";
   set name(String newValue) {
     // hazle algo raro
     _name = newValue;
